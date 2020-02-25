a54f07b8298758ed26b7cf0747fe690f8d59a27f
langren1353 <1353464539@qq.com>
2020-02-25 23:00:41 +0800

    修改页面显示效果 & 修正部分标志 && 以15%丢包为严重

--- web/js/serverstatus.js
+++ web/js/serverstatus.js
@@ -371,7 +371,7 @@ function uptime() {
                 var PING_189 = result.servers[i].ping_189.toFixed(0);
                 var PING_10086 = result.servers[i].ping_10086.toFixed(0);
                 var pingTime = result.servers[i].time_10010 + "ms💻" + result.servers[i].time_189 + "ms💻" + result.servers[i].time_10086 + "ms"
-                if (PING_10010 >= 10 || PING_189 >= 10 || PING_10086 >= 10)
+                if (PING_10010 >= 15 || PING_189 >= 15 || PING_10086 >= 15)
                     TableRow.children["ping"].children[0].children[0].className = "progress-bar progress-bar-danger";
                 else
                     TableRow.children["ping"].children[0].children[0].className = "progress-bar progress-bar-success";
